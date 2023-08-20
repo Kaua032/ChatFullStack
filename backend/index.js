7107724f318c8ca397da910fcb9475fd34253ea6
Kauã Xavier <kauaxavier032@gmail.com>
2023-08-20 09:58:08 -0300

    feat: add chatengine.io

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,17 @@ app.use(cors({origin: true}));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username : username, secret: "sha256..."})
-})
 
-app.listen(3001);
+    try{
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username : username, secret : username, first_name: username},
+            {headers: {"private-key" : "0d648eaf-8d67-4c65-8bb2-b7fe1144fec9"}}
+        )
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(2023);
